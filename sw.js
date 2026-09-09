ef440d279735807f0c8ec74776387ad9d27c3679
triartst-svg <triartst@gmail.com>
2026-09-10 00:49:12 +0300

    Cache split app files

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "kovbasa-godyny-v1";
-const ASSETS = ["./","./index.html","./manifest.json","./icon.svg"];
+const CACHE = "kovbasa-godyny-v2";
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
